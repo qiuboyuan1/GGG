06a78fb464e396f6e49f3399b0e4a4688fa7655d
qiuboyuan <1124209226@qq.com>
2016-08-11 18:05:09 +0800

    阿达

--- app/src/main/java/com/qiu/ggg/MainActivity.java
+++ app/src/main/java/com/qiu/ggg/MainActivity.java
@@ -1,10 +1,16 @@
 package com.qiu.ggg;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private CustomVideoView videoview;
+    private Button btn_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +19,35 @@ public class MainActivity extends AppCompatActivity {
 
 
         //asdfasdfasdf
+
+        initView();
+    }
+
+    private void initView() {
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(this);
+
+        videoview = (CustomVideoView) findViewById(R.id.videoview);
+        //设置播放加载路径
+        videoview.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.guide));
+        //播放
+        videoview.start();
+        //循环播放
+        videoview.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                videoview.start();
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_start:
+                Toast.makeText(this,"进入了主页",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
